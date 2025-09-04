9731d1de16587579c7933ee89392d0203fbe79cc
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-04 17:26:37 +0900

    feat: lab

--- StoryboardLab/ViewController.swift
+++ StoryboardLab/ViewController.swift
@@ -12,11 +12,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func showActivityView(_ sender: UIBarButtonItem) {
         let controller = UIActivityViewController(
-            activityItems: [imageView.image!], applicationActivities: nil
+            activityItems: [imageView.image!],
+            applicationActivities: nil
         )
         self.present(controller, animated: true, completion: nil)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
