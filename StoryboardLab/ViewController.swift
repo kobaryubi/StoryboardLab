eba83e29027bc450b3d092136b14d1c1260dbceb
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-04 12:34:08 +0900

    feat: lab

--- StoryboardLab/ViewController.swift
+++ StoryboardLab/ViewController.swift
@@ -3,7 +3,9 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBAction func sayHello() {
+        label.text = "Hello, World!"
     }
+    @IBOutlet weak var sliderLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
