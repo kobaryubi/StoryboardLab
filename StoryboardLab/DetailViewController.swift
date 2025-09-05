547a3466b99d94ef3f672d8444341648bdf4ba63
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-05 16:48:41 +0900

    feat: lab

--- StoryboardLab/DetailViewController.swift
+++ StoryboardLab/DetailViewController.swift
@@ -2,5 +2,15 @@ import UIKit
 import WebKit
 
 class DetailViewController: UIViewController {
+    var link:String!
+    
     @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = URL(string: self.link) {
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }
+    }
 }
