b2f2de50154a3f5ccc3e5b4f50b3fa77261fcbcc
Masahiko Kobayashi <kobaryubi6@gmail.com>
2025-09-05 12:17:48 +0900

    feat: lab

--- StoryboardLab/ListViewController.swift
+++ StoryboardLab/ListViewController.swift
@@ -1,1 +1,21 @@
-import Foundation
+import UIKit
+
+class ListViewController: UITableViewController {
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 3
+    }
+
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "Cell",
+            for: indexPath
+        )
+        return cell
+    }
+}
